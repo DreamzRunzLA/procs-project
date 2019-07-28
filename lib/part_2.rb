ed0e48c396b08741cb0c98ab9b578f4126bf8c31
DreamzRunzLA <salmaan.a.qadir@gmail.com>
2019-07-27 19:22:04 -0600

    Finished procs exercise

--- lib/part_2.rb
+++ lib/part_2.rb
@@ -1,29 +1,46 @@
 def reverser(str, &proc)
-    # reversed = str.reverse!
-    # ans = ''
-    # reversed.each_char do |char|
-    #     ans += proc.call(char)
-    # end
-    # return ans
+    reversed = str.reverse!
+    return proc.call(reversed)
 end
 
 
 def word_changer(sent, &proc)
+    words = sent.split(' ')
+    ans = []
 
+    words.each do |ele|
+        ans << proc.call(ele)
+    end
+
+    return ans.join(' ')
 end
 
 
-def greater_proc_value(num, &proc1, &proc2)
-
+def greater_proc_value(num, proc1, proc2)
+    return proc1.call(num) > proc2.call(num) ? proc1.call(num) : proc2.call(num)
 end
 
 
-def and_selector(arr, &proc1, &proc2)
-
+def and_selector(arr, proc1, proc2)
+    ans = []
+    arr.each do |ele|
+        if proc1.call(ele) && proc2.call(ele)
+            ans << ele
+        end
+    end
+    return ans
 end
 
 
-def alternating_mapper(arr, &proc1, &proc2)
-
+def alternating_mapper(arr, proc1, proc2)
+    ans =[]
+    arr.each_with_index do |ele, i|
+        if i % 2 == 0
+            ans << proc1.call(ele)
+        else
+            ans << proc2.call(ele)
+        end
+    end
+    return ans
 end
 
